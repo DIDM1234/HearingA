7f76e853018919f5a9cb81d737c0b52b66f40c72
tamnuwat <tamnuwat.val@gmail.com>
2018-10-31 17:46:29 +0700

    10/31/2018 5.45

--- app/src/main/java/com/example/sujin/hearinga/HearingTest.java
+++ app/src/main/java/com/example/sujin/hearinga/HearingTest.java
@@ -1,13 +1,17 @@
 package com.example.sujin.hearinga;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HearingTest extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +20,29 @@ public class HearingTest extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mButton = (Button) findViewById(R.id.okbutton);
+        mButton.setOnClickListener(mbuttonOnClick);
+
     }
 
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    View.OnClickListener mbuttonOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openSaveProfileActivity();
+        }
+    };
+
+    private void openSaveProfileActivity()
+    {
+        Intent myIntent = new Intent(this,SaveProfile.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
 }
